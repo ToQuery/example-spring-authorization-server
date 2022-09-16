488f1cd41a2964f61d9fda1a5351f29176bec3d7
ToQuery <toquery@qq.com>
2022-09-16 16:05:24 +0800

    init

---
+++ src/main/java/io/github/toquery/example/spring/authorization/server/ExampleSpringAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/example/spring/authorization/server/ExampleSpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.authorization.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
